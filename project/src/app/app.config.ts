8fbc0fa9c68aa12e3170701b2768d4f384f104a9
m.iglovskiy <m.iglovskiy@tbank.ru>
2025-12-17 01:18:45 +0700

    fire safe

--- project/src/app/app.config.ts
+++ project/src/app/app.config.ts
@@ -8,23 +8,14 @@ import { initializeApp } from 'firebase/app';
 import { provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { appRoutes } from './app.routes';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDvYOz2bIQUA8j4FAOhpe6h3BWbohun34g',
-  authDomain: 'mws-ai.firebaseapp.com',
-  projectId: 'mws-ai',
-  storageBucket: 'mws-ai.firebasestorage.app',
-  messagingSenderId: '202240356106',
-  appId: '1:202240356106:web:e42a9b7ddd37b8e7b614f3',
-  measurementId: 'G-4T6MVE4EPD',
-};
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
   ],
 };
